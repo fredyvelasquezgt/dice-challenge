e9050c4d10f268c8cadd39f5e7027902e28d2aaf
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-09 12:36:37 -0600

    added styles to RollDice

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Die from './Die'
+import './RollDice.css'
 
 class RollDice extends Component {
     static defaultProps = {
@@ -17,9 +18,12 @@ class RollDice extends Component {
         this.setState({die1: newDie1, die2: newDie2})
     }
     render() {
-        return <div>
+        return <div className="RollDice">
+            <div className="RollDice-container" >
             <Die face={this.state.die1} />
-            <Die face={this.state.die2} />
+            <Die face={this.state.die2} />      
+            </div>
+        
             <button onClick={this.roll} >Roll Dice</button>
 
         </div>
