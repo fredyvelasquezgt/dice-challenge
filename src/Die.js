36ccd234b2e4899429c483a19b450e269d84d06a
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-09 13:44:44 -0600

    finished project

--- src/Die.js
+++ src/Die.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import './Die.css'
+import React, { Component } from "react";
+import "./Die.css";
 
 class Die extends Component {
-    render() {
-        return <i className={`Die fas fa-dice-${this.props.face}`}></i>
-    }
+  render() {
+    return (
+      <i
+        className={`Die fas fa-dice-${this.props.face} ${this.props.rolling &&
+          "shaking"}`}
+      />
+    );
+  }
 }
-
 export default Die;
