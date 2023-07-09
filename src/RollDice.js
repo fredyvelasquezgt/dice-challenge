894ade5449c33c7c3f37a6e4a5aa23a3a6ffbcb9
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-09 13:41:19 -0600

    wobble

--- src/RollDice.js
+++ src/RollDice.js
@@ -8,14 +8,18 @@ class RollDice extends Component {
     }
     constructor(props) {
         super(props)
-        this.state = {die1: 'one', die2:'one'}
+        this.state = {die1: 'one', die2:'one', rolling: false}
         this.roll = this.roll.bind(this)
     }
     roll() {
         const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
         const newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
 
-        this.setState({die1: newDie1, die2: newDie2})
+        this.setState({die1: newDie1, die2: newDie2, rolling:true})
+
+        setTimeout(() => {
+            this.setState({rolling: false})
+        }, 1000)
     }
     render() {
         return <div className="RollDice">
@@ -24,7 +28,9 @@ class RollDice extends Component {
             <Die face={this.state.die2} />      
             </div>
         
-            <button onClick={this.roll} >Roll Dice</button>
+            <button onClick={this.roll} disabled={this.state.rolling} >
+                {this.state.rolling ? 'Rolling...' : 'Roll dice'}
+            </button>
 
         </div>
     }
