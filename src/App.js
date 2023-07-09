0bfb7fad76aceb302dabd7a267f03a3d05e18637
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-09 12:01:28 -0600

    render first dice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Die from './Die';
+
 
 function App() {
   return (
     <div className="App">
-    
+      <Die />
     </div>
   );
 }
